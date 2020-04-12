4bdfbc1a01e6e2709ba4430af767c60f6edbe87a
ZhangXin <669412425@qq.com>
2020-04-12 20:39:13 +0800

    'new'

--- src/main/java/com/zx/springbootjsp/controller/HelloController.java
+++ src/main/java/com/zx/springbootjsp/controller/HelloController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     // 该方法使用hello请求无法获得，因为指定的前缀为/WEB-INF/，该文件在webapp目录下，可以使用hello.jsp访问
+    // 项目启动后会自动在打开浏览器，直接在地址栏中添加hello.jsp就可访问，表示直接访问hello.jsp
     @RequestMapping("/hello")
     public String hello(){
     System.out.println("hello");
     return "hello";
 }
-    // 该方法可以使用success请求获得，不能使用success.jsp访问，因为配置的web资源在webapp目录下
+    // 该方法可以使用success请求获得，不能使用success.jsp访问，因为配置的web资源在webapp/WEB-INF下
     @RequestMapping("/success")
     public String success(){
         System.out.println("success");
